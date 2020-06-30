b6699b866a428c7307de45a0b16aca0b08dbe91d
TonHaver <64653567+TonHaver@users.noreply.github.com>
2020-06-30 22:52:10 +0200

    Update home.php

--- pages/home.php
+++ pages/home.php
@@ -14,5 +14,5 @@
         STEP is een projectorkest dat zich richt op orkestwerk voor kleine, unieke bezettingen. Van oude én
         nieuwe componisten.
     </p>
-    <a class='waves-effect waves-light btn btnwhite' href="/meedoen">doe mee</a>
+    <a class='waves-effect waves-light btn btnwhite' href="/index.php?pagina=meedoen">doe mee</a>
 </div>
