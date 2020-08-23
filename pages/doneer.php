4fe2bf78a2dcffca6a468ccf1051560845dbf9cc
STEP <66801337+STEP-account@users.noreply.github.com>
2020-08-23 17:10:21 +0200

    Update doneer rekeningnummer opmaak

--- pages/doneer.php
+++ pages/doneer.php
@@ -3,8 +3,8 @@
 <p class="textblack">Momenteel werkt STEP hard aan het verkrijgen van een ANBI status. We hopen
 binnenkort uitleg te geven hoe u ook fiscaal voordelig kunt schenken aan onze stichting.
 
-Wilt u nu al een donatie geven (als bedrijf 
-of particulier), dan kan dat op ons rekeningnummer NL26 TRIO 0320017605 t.n.v. Stichting Ensemble Project met als beschrijving Donatie
+Wilt u nu al een donatie geven (als bedrijf of particulier),
+dan kan dat op ons rekeningnummer NL26 TRIO 0320017605 t.n.v. Stichting Ensemble Project met als beschrijving Donatie
 of neem contact op met <a href="mailto:acquisiteur@ensembleproject.nl">acquisiteur@ensembleproject.nl</a></p>
 </div>
 
